9ac887728869d5b484e159e106f187f6e77e5064
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-06-12 11:13:46 -0600

    Completed front-end log in

--- RamenAI/views.py
+++ RamenAI/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for, abort, flash, session, g
 from .app import app
 
+
 @app.route("/")
 def login():
     return render_template("login.html")
