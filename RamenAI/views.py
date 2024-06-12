eb3616206bfc4696e2f037bb441b70d972f2b5aa
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-06-12 14:00:22 -0600

    Added favicon and post and get for login

--- RamenAI/views.py
+++ RamenAI/views.py
@@ -1,7 +1,25 @@
 from flask import render_template, request, redirect, url_for, abort, flash, session, g
 from .app import app
 
+""" def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if 'uid' not in session:
+            flash("Por favor ingresa a sesión")
+            return redirect(url_for('login_get'))
+
+        return view(**kwargs)
+
+    return wrapped_view """
+
+@app.get("/login")
+def login_get():
+    return render_template("login.html")
+
+@app.post("/login")
+def login_post():
+    return redirect(url_for('chat', _method="GET"))
 
 @app.route("/")
-def login():
-    return render_template("login.html")
+def chat():
+    return render_template("chat.html")
